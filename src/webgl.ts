e0ee877edcf98aae557ffd6e44b4000afb0a2faf
Filipe Santiago <feelsantiago@Filipes-MacBook-Pro.local>
2020-09-30 21:12:36 -0300

    change square to cube and add another rotation

--- src/webgl.ts
+++ src/webgl.ts
@@ -3,6 +3,7 @@ import { mat4 } from 'gl-matrix';
 interface PositionBuffer {
     position: WebGLBuffer;
     color: WebGLBuffer;
+    indices: WebGLBuffer;
 }
 
 interface Shaders {
@@ -22,7 +23,7 @@ interface ProgramInfo {
     };
 }
 
-let squareRotation = 0.0;
+let cubeRotation = 0.0;
 
 const fetchFile = async (url: string): Promise<string> => {
     const stream = await fetch(url);
@@ -91,40 +92,97 @@ const initBuffers = (gl: WebGLRenderingContext): PositionBuffer => {
     // Select the positionBuffer as the one to apply buffer
     gl.bindBuffer(gl.ARRAY_BUFFER, positionBuffer);
 
-    // Square Positions
-    const positions = [-1.0, 1.0, 1.0, 1.0, -1.0, -1.0, 1.0, -1.0];
+    // Cube Positions
+    // Each vertex is shared by 3 faces, we just need 8 vertex to create the cube
+    // But we want each face to have a differ color, so we need to create a copy of each vertex to not share the color
+    // OpenGL set one color for each vertex only
+    // prettier-ignore
+    const positions = [
+        // Front face
+        -1.0, -1.0,  1.0,
+        1.0, -1.0,  1.0,
+        1.0,  1.0,  1.0,
+        -1.0,  1.0,  1.0,
+
+        // Back face
+        -1.0, -1.0, -1.0,
+        -1.0,  1.0, -1.0,
+        1.0,  1.0, -1.0,
+        1.0, -1.0, -1.0,
+
+        // Top face
+        -1.0,  1.0, -1.0,
+        -1.0,  1.0,  1.0,
+        1.0,  1.0,  1.0,
+        1.0,  1.0, -1.0,
+
+        // Bottom face
+        -1.0, -1.0, -1.0,
+        1.0, -1.0, -1.0,
+        1.0, -1.0,  1.0,
+        -1.0, -1.0,  1.0,
+
+        // Right face
+        1.0, -1.0, -1.0,
+        1.0,  1.0, -1.0,
+        1.0,  1.0,  1.0,
+        1.0, -1.0,  1.0,
+
+        // Left face
+        -1.0, -1.0, -1.0,
+        -1.0, -1.0,  1.0,
+        -1.0,  1.0,  1.0,
+        -1.0,  1.0, -1.0,
+    ];
 
     // Now pass the list of positions into WebGL to build the
     // shape. We do this by creating a Float32Array from the
     // JavaScript array, then use it to fill the current buffer.
     gl.bufferData(gl.ARRAY_BUFFER, new Float32Array(positions), gl.STATIC_DRAW);
 
-    const colors = [
-        1.0,
-        1.0,
-        1.0,
-        1.0, // white
-        1.0,
-        0.0,
-        0.0,
-        1.0, // red
-        0.0,
-        1.0,
-        0.0,
-        1.0, // green
-        0.0,
-        0.0,
-        1.0,
-        1.0, // blue
+    const faceColors = [
+        [1.0, 1.0, 1.0, 1.0], // Front face: white
+        [1.0, 0.0, 0.0, 1.0], // Back face: red
+        [0.0, 1.0, 0.0, 1.0], // Top face: green
+        [0.0, 0.0, 1.0, 1.0], // Bottom face: blue
+        [1.0, 1.0, 0.0, 1.0], // Right face: yellow
+        [1.0, 0.0, 1.0, 1.0], // Left face: purple
     ];
+
+    // Convert the array of colors into a table for all the vertices.
+    let colors = [];
+
+    for (let color of faceColors) {
+        // Repeat each color four times for the four vertices of the face
+        colors = colors.concat(color, color, color, color);
+    }
 
     const colorBuffer = gl.createBuffer();
     gl.bindBuffer(gl.ARRAY_BUFFER, colorBuffer);
     gl.bufferData(gl.ARRAY_BUFFER, new Float32Array(colors), gl.STATIC_DRAW);
 
+    // This array defines each face as two triangles, using the
+    // indices into the vertex array to specify each triangle's
+    // position.
+
+    // prettier-ignore
+    const indices = [
+        0,  1,  2,      0,  2,  3,    // front
+        4,  5,  6,      4,  6,  7,    // back
+        8,  9,  10,     8,  10, 11,   // top
+        12, 13, 14,     12, 14, 15,   // bottom
+        16, 17, 18,     16, 18, 19,   // right
+        20, 21, 22,     20, 22, 23,   // left
+    ];
+
+    const indexBuffer = gl.createBuffer();
+    gl.bindBuffer(gl.ELEMENT_ARRAY_BUFFER, indexBuffer);
+    gl.bufferData(gl.ELEMENT_ARRAY_BUFFER, new Uint16Array(indices), gl.STATIC_DRAW);
+
     return {
         position: positionBuffer,
         color: colorBuffer,
+        indices: indexBuffer,
     };
 };
 
@@ -170,14 +228,16 @@ const drawScene = (gl: WebGLRenderingContext, programInfo: ProgramInfo, buffers:
     mat4.rotate(
         modelViewMatrix, // destination matrix
         modelViewMatrix, // matrix to rotate
-        squareRotation, // amount to rotate in radians
-        [0, 0, 1]
-    ); // axis to rotate around
+        cubeRotation, // amount to rotate in radians
+        [0, 0, 1] // axis to rotate around
+    );
+
+    mat4.rotate(modelViewMatrix, modelViewMatrix, cubeRotation * 0.7, [0, 1, 0]);
 
     // Tell WebGL how to pull out the positions from the position
     // buffer into the vertexPosition attribute.
     {
-        const numComponents = 2; // pull out 2 values per iteration
+        const numComponents = 3; // pull out 2 values per iteration
         const type = gl.FLOAT; // the data in the buffer is 32bit floats
         const normalize = false; // don't normalize
         const stride = 0; // how many bytes to get from one set of values to the next
@@ -208,6 +268,9 @@ const drawScene = (gl: WebGLRenderingContext, programInfo: ProgramInfo, buffers:
         gl.enableVertexAttribArray(programInfo.attribLocations.vertexColor);
     }
 
+    // Tell WebGL which indices to use to index the vertices
+    gl.bindBuffer(gl.ELEMENT_ARRAY_BUFFER, buffers.indices);
+
     // Tell WebGL to use our program when drawing
     gl.useProgram(programInfo.program);
 
@@ -216,12 +279,13 @@ const drawScene = (gl: WebGLRenderingContext, programInfo: ProgramInfo, buffers:
     gl.uniformMatrix4fv(programInfo.uniformLocations.modelViewMatrix, false, modelViewMatrix);
 
     {
+        const vertexCount = 36;
+        const type = gl.UNSIGNED_SHORT;
         const offset = 0;
-        const vertexCount = 4;
-        gl.drawArrays(gl.TRIANGLE_STRIP, offset, vertexCount);
+        gl.drawElements(gl.TRIANGLES, vertexCount, type, offset);
     }
 
-    squareRotation += deltaTime;
+    cubeRotation += deltaTime;
 };
 
 const main = async () => {
@@ -263,7 +327,7 @@ const main = async () => {
         drawScene(gl, programInfo, buffer, deltaTime);
 
         requestAnimationFrame(render);
-    }
+    };
 
     requestAnimationFrame(render);
 };
